36f69806e9a1f96f86744ed9bccc4aee091494c9
wandi-create <hrey5526@gmail.com>
2025-11-14 10:34:09 +0700

    Update image URL for Ikhwan certificate template

--- assets/Syahadah_template_ikhwan.png.tsx
+++ assets/Syahadah_template_ikhwan.png.tsx
@@ -1,5 +1,5 @@
 // This file represents the image asset for the Ikhwan (male) certificate template.
 // 1. Upload your 'Syahadah_template_ikhwan.png' to an image hosting service (like imgur.com).
 // 2. Paste the public URL for your image below, replacing the placeholder text.
-const path = 'URL_GAMBAR_IKHWAN_ANDA_DISINI';
+const path = 'https://imgur.com/Sbj4bvn';
 export default path;
