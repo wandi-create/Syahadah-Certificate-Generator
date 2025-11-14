c69deb662868d8fc6fc0d8181edcdd8ee5960d0d
wandi-create <hrey5526@gmail.com>
2025-11-14 10:33:14 +0700

    Update image URL for Akhwat certificate template

--- assets/Syahadah_template_akhwat.png.tsx
+++ assets/Syahadah_template_akhwat.png.tsx
@@ -1,5 +1,5 @@
 // This file represents the image asset for the Akhwat (female) certificate template.
 // 1. Upload your 'Syahadah_template_akhwat.png' to an image hosting service (like imgur.com).
 // 2. Paste the public URL for your image below, replacing the placeholder text.
-const path = 'URL_GAMBAR_AKHWAT_ANDA_DISINI';
+const path = 'https://imgur.com/Cze2L6Z';
 export default path;
